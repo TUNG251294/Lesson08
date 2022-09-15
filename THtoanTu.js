0f0d9605e0f63d00f57f6a24ce87bbba7be4dfa6
TUNG251294 <huynhthanhtunga1@gmail.com>
2022-09-16 05:29:48 +0700

    feature01

---
+++ THtoanTu.js
@@ -0,0 +1,4 @@
+let a = parseInt(<input type="number" placeholder="Nhập số a">);
+    let b = parseInt(<input type="number" placeholder="Nhập số b">);
+        if(a + b < 4){document.write("Below")}
+        else {document.write("Over")}
---
+++ THtoanTu2.js
@@ -0,0 +1,4 @@
+let a = parseInt(prompt("Nhập số a"))
+let b = parseInt(prompt("Nhập số b"))
+if (a+b<4){alert("Below");}
+else {alert("Over");}
